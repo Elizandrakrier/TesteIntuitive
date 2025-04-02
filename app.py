a0ea4697d2086b8a9425e7a49682b0b23fc96765
Elizandra Krier <90136069+Elizandrakrier@users.noreply.github.com>
2025-04-02 18:31:45 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+﻿# pylint: disable=astroid-error
+import pandas as pd
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+df = pd.read_csv("Relatorio_cadop.csv", sep=",", encoding="latin1")
+
+
+@app.route("/buscar", methods=["GET"])
+def buscar_operadoras():
+    termo = request.args.get("termo", "").lower()
+
+    resultados = df[df["Razao_Social"].str.lower().str.contains(termo)]
+
+    return jsonify(resultados.to_dict(orient="records"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
